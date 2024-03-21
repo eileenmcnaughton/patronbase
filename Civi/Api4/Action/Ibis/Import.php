91d42403deeac4192899a0eb342354e30d5e8312
eileen <emcnaughton@wikimedia.org>
2024-03-21 17:46:50 +1300

    Cleanup

--- Civi/Api4/Action/Ibis/Import.php
+++ Civi/Api4/Action/Ibis/Import.php
@@ -70,14 +70,12 @@ class Import extends ImportBaseAction {
           $record['Item type'],
         ]);
         $lineTotal = \CRM_Utils_Rule::cleanMoney($record['Amount inc']);
-        $isNegative = FALSE;
+
         if (str_starts_with($lineTotal, '(')) {
-          $isNegative = TRUE;
           $lineTotal = -substr($lineTotal, 1, -1);
         }
         $quantity = $record['Units'];
         if (str_starts_with($quantity, '(')) {
-          $isNegative = TRUE;
           $quantity = substr($quantity, 1, -1);
         }
         $quantity = (float) $quantity;
