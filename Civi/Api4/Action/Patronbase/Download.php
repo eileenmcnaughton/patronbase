ffd05f3d2db861cacb0f9eb6b87b2eef018aaec3
eileen <emcnaughton@wikimedia.org>
2024-09-18 12:49:52 +1200

    in_array fix

--- Civi/Api4/Action/Patronbase/Download.php
+++ Civi/Api4/Action/Patronbase/Download.php
@@ -62,7 +62,7 @@ class Download extends ImportBaseAction {
         foreach ($attachments as $attachment) {
           $fileName = basename($attachment['fullName']);
           \Civi::log('ibis')->info('processing attachment' . $fileName);
-          if (in_array(['ibis.csv', 'patronbase.csv'], $fileName, TRUE)) {
+          if (in_array($fileName, ['ibis.csv', 'patronbase.csv'], TRUE)) {
             \Civi::log('ibis')->info('storing file ' . $path . $fileName);
             rename($attachments[0]['fullName'], $path . $fileName);
           }
